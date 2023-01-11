720171b54ef0075b8e4d0113544d90285458ce8e
Q-engineering <info@qengineering.eu>
2023-01-11 18:48:34 +0100

    C++ header files

--- include/Tjson.h
+++ include/Tjson.h
@@ -13,16 +13,21 @@ public:
     ~Tjson();
     bool LoadFromFile(const std::string FileName);
     bool GetSettings(void);
-    std::string Version;    //Version string
-    std::string Gstr;       //Gstreamer string
-    std::string Cstr;       //Car darknet model file name
-    std::string Lstr;       //License darknet model file name
-    std::string Ostr;       //OCR darknet model file name
-    bool     PrintOn;       //Show license plate on teminal
+    std::string  Version;   //Version string
+    std::string  Gstr;      //Gstreamer string
+    std::string  Cstr;      //Car darknet model file name
+    std::string  Lstr;      //License darknet model file name
+    std::string  Ostr;      //OCR darknet model file name
+    bool      PrintOn;      //Show license plate on teminal
+    bool HeuristicsOn;      //Sort character position and remove doubles
+    double   ThresCar;      //threshold detection of car model
+    double ThresPlate;      //threshold detection of license plate model
+    double   ThresOCR;      //threshold detection of ocr model
 protected:
     bool GetSetting(const std::string Key,int& Value);
     bool GetSetting(const std::string Key,bool& Value);
     bool GetSetting(const std::string Key,std::string& Value);
+    bool GetSetting(const std::string Key,double& Value);
 private:
     json j;
     bool Jvalid;
