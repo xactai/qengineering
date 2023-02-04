e24766ed6536119c1a6b585e6c5600dcaf810be5
Q-engineering <info@qengineering.eu>
2023-02-04 11:54:16 +0100

    C++ header files

--- include/RTSPcam.h
+++ include/RTSPcam.h
@@ -6,22 +6,39 @@
 #ifndef RTSPCAM_H
 #define RTSPCAM_H
 
+#include "dirent.h"
 #include <opencv2/opencv.hpp>
-
 
 class RTSPcam
 {
 public:
-    RTSPcam(const std::string& filename, int apiPreference = cv::CAP_ANY);
+    RTSPcam(void);
     virtual ~RTSPcam();
+
+    //used with Buster or Bullseye+Legacy Video Stack (0 opens RaspiCam)
+    void Open(const int Value);
+    //GStreamer or FFmpeg pipeline
+    void Open(const std::string& MyString, const int apiPreference = cv::CAP_ANY);
+
+    //get the lateste frame without (almost no letency)
     bool GetLatestFrame(cv::Mat& frame);
-    bool Picture;       // true when a jpg or png picture is loaded.
-    double FrameTime;   // 1/FPS
+
+    double FPS;                     // FPS
+    bool UsePicture;                // true when a jpg, png or bmp picture is loaded.
+    bool UseFolder;                 // true when a only folder name is loaded.
+    std::string CurrentFileName;    // get the name of the picture or frame number
 protected:
-    cv::VideoCapture cap;
+    cv::VideoCapture* cap;
+    bool FirstPic;
+    size_t FrameCnt;
     std::string MyFile;
 private:
+    DIR *dir;
+    struct dirent *ent;
+    double FrameTime;               // 1/FPS
+    void ProcessOpen(void);
+    void NiceString(const size_t Cnt);
+    void NiceString(const std::string Str);
     std::chrono::steady_clock::time_point Tgrab;
 };
-
 #endif // RTSPCAM_H
--- include/Tjson.h
+++ include/Tjson.h
@@ -1,10 +1,30 @@
 #ifndef TJSON_H
 #define TJSON_H
 
+#include <opencv2/opencv.hpp>
 #include <nlohmann/json.hpp>
+#include <string>
+#include <fstream>
 
 using json = nlohmann::json;
 
+//----------------------------------------------------------------------------------------
+inline bool FileExists(const std::string& name)
+{
+    if(FILE *file = fopen(name.c_str(), "r")){
+        fclose(file);
+        return true;
+    }
+    else return false;
+}
+//----------------------------------------------------------------------------------------
+inline void MakeDir(const std::string& folder)
+{
+    if(folder!="none"){
+        std::string Str = "mkdir -p "+folder;
+        system(Str.c_str());
+    }
+}
 //----------------------------------------------------------------------------------------
 class Tjson
 {
@@ -13,24 +33,33 @@ public:
     ~Tjson();
     bool LoadFromFile(const std::string FileName);
     bool GetSettings(void);
-    std::string  Version;   //Version string
-    std::string  Gstr;      //Gstreamer string
-    std::string  Cstr;      //Car darknet model file name
-    std::string  Lstr;      //License darknet model file name
-    std::string  Ostr;      //OCR darknet model file name
-    bool      PrintOn;      //Show license plate on teminal
-    bool HeuristicsOn;      //Sort character position and remove doubles
-    double   ThresCar;      //threshold detection of car model
-    double ThresPlate;      //threshold detection of license plate model
-    double   ThresOCR;      //threshold detection of ocr model
-protected:
-    bool GetSetting(const std::string Key,int& Value);
-    bool GetSetting(const std::string Key,bool& Value);
-    bool GetSetting(const std::string Key,std::string& Value);
-    bool GetSetting(const std::string Key,double& Value);
+    void MakeFolders(void);
+    std::string  Version;     //Version string
+    std::string  Gstr;        //Gstreamer string
+    std::string  Cstr;        //Car darknet model file name
+    std::string  Lstr;        //License darknet model file name
+    std::string  Ostr;        //OCR darknet model file name
+    bool   PrintOnCli;        //Show license plate on teminal
+    bool PrintOnRender;       //show license plate in window
+    bool HeuristicsOn;        //Sort character position and remove doubles
+    double   ThresCar;        //threshold detection of car model
+    double ThresPlate;        //threshold detection of license plate model
+    double   ThresOCR;        //threshold detection of ocr model
+    std::string FoI_Folder;   //directory with analysed frames
+    std::string Car_Folder;   //directory with analysed cars
+    std::string Plate_Folder; //directory with analysed plates
+    std::string Json_Folder;  //directory with json outputs
+    std::string Render_Folder; //directory with ocr results
+    int MJPEG_Port;           //output stream (0 = no stream, usually 8090)
+    cv::Rect RoiCrop;         //cropped roi (width==0 and/or height==0 no cropping)
+
 private:
     json j;
     bool Jvalid;
+    bool GetSetting(const json& s, const std::string Key, int& Value);
+    bool GetSetting(const json& s, const std::string Key, bool& Value);
+    bool GetSetting(const json& s, const std::string Key, double& Value);
+    bool GetSetting(const json& s, const std::string Key, std::string& Value);
 };
 //----------------------------------------------------------------------------------------
 
